84656326ce92a880313395473d5a09d5f082853b
jess <jlfinlayson4@gmail.com>
2019-08-28 14:19:19 -0400

    Updates

---
+++ precache-manifest.f2040113eb3449d9de083f58011ee9bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac594bb8b86dcd62756c8097096770cd",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e703dc05ac40a1ffb020",
+    "url": "/clicky-game/static/css/main.b9d1d6bd.chunk.css"
+  },
+  {
+    "revision": "13255a7a3b6ba471b8a2",
+    "url": "/clicky-game/static/js/2.f8f6ec76.chunk.js"
+  },
+  {
+    "revision": "e703dc05ac40a1ffb020",
+    "url": "/clicky-game/static/js/main.44df8113.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  }
+]);
